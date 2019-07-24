635594608775d9f943e9a0b9e8f9eadb3bb9b5be
huynhtandai95 <36435011+huynhtandai95@users.noreply.github.com>
2019-07-24 17:49:51 +0700

    Update th4.js

--- 2-Bien_Kieudulieu_Toantu/th4.js
+++ 2-Bien_Kieudulieu_Toantu/th4.js
@@ -1,7 +1,7 @@
 
     let inputWidth=prompt("Nhập chiều rộng hình chữ nhật");
     let inputHeight=prompt("Nhập chiều cao hình chữ nhật");
-    let width = parseInt(inputWidth);      //parseInt chuyển chuỗi sang số
+    let width = parseInt(inputWidth);      //parseInt chuyển kiểu dữ liệu từ chuỗi sang số nguyên
     let height = parseInt(inputHeight);
     let area= width*height;
     document.write("Diện tích hình chữ nhật: " + area);
