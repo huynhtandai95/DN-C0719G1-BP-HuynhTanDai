e6718e34811777ca31d87546061935074ec3c368
Huynh Tan Dai <huynhtandai95@gmail.com>
2019-07-24 17:46:14 +0700

    update

---
+++ 2-Bien_Kieudulieu_Toantu/th4.js
@@ -0,0 +1,9 @@
+
+    let inputWidth=prompt("Nhập chiều rộng hình chữ nhật");
+    let inputHeight=prompt("Nhập chiều cao hình chữ nhật");
+    let width = parseInt(inputWidth);
+    let height = parseInt(inputHeight);
+    let area= width*height;
+    document.write("Diện tích hình chữ nhật: " + area);
+
+
